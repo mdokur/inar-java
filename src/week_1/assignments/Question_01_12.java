bb0e3212b9629c606fe2682e346fafed6e79291c
mdokur <m.denisokur@gmail.com>
2023-08-11 11:47:47 +0200

    yine deneme

---
+++ src/week_1/assignments/Question_01_12.java
@@ -0,0 +1,12 @@
+package week_1.assignments;
+
+public class Question_01_12 {
+    public static void main(String [] args){
+        System.out.println("Distance in miles:");
+        System.out.println("24.0");
+        System.out.println("Time in minutes:");
+        System.out.println("100 minutes 35 seconds");
+        System.out.println("Average speed in kilometers per hour:");
+        System.out.println((24.0*1.6)/((1.0+(40.0/60.0)+(35.0/3600.0))));
+    }
+}
