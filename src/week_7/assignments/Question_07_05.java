77edc9ef5d4ca20524e187b4b788a7bbbb459c7d
mdokur <m.denisokur@gmail.com>
2023-10-04 23:24:41 +0200

    uptaded

---
+++ src/week_7/assignments/Question_07_05.java
@@ -0,0 +1,29 @@
+package week_7.assignments;
+
+import java.util.Scanner;
+
+public class Question_07_05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter ten numbers: ");
+        int[] count = new int[10];
+        for (int i = 0; i < 10; i++) {
+            int inputNumber = input.nextInt();
+            count[inputNumber - 1]++;
+        }
+        printDistinctNumbers(count);
+    }
+
+    public static void printDistinctNumbers(int[] array) {
+        System.out.print("The distinct numbers are: ");
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                System.out.print(i + 1 + " ");
+                count++;
+            }
+        }
+        System.out.println("\nThe number of distinct numbers is: " + count);
+    }
+}
---
+++ src/week_7/assignments/Question_07_10.java
@@ -0,0 +1,7 @@
+package week_7.assignments;
+
+public class Question_07_10 {
+    public static void main(String[] args) {
+
+    }
+}
