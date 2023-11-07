fda91cd9160b7ab00392fb181f9fb07371dc2a51
mdokur <m.denisokur@gmail.com>
2023-11-07 17:04:53 +0100

    changes

---
+++ src/week_10/assigments/Question_10_01.java
@@ -0,0 +1,11 @@
+package week_10.assigments;
+
+public class Question_10_01 {
+    public static void main(String[] args) {
+        Time currentTime = new Time();
+        Time time1 = new Time(555550000);
+
+        System.out.println(currentTime.getHour()+":" + currentTime.getMinute() + ":" + currentTime.getSecond());
+        System.out.println(time1.getHour()+":"+ time1.getMinute()+":" + time1.getSecond());
+    }
+}
---
+++ src/week_10/assigments/Time.java
@@ -0,0 +1,40 @@
+package week_10.assigments;
+
+public class Time {
+    private long hour;
+    private long minute;
+    private long second;
+
+    public Time() {
+        long totalSecond = System.currentTimeMillis() / 1000;
+        second = (int) (totalSecond % 60);
+        minute = (int) ((totalSecond / 60) % 60);
+        hour = (int) ((totalSecond / 3600) % 24);
+    }
+
+    public Time(int hour, int minute, int second) {
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+    }
+
+    public Time(long elapsedTime) {
+        long totalSecond = elapsedTime / 1000;
+        second = (int) (elapsedTime % 60);
+        minute = (int) ((elapsedTime / 60) % 60);
+        hour = (int) ((elapsedTime / 3600) % 24);
+    }
+    public long getHour(){
+        return hour;
+    }
+    public long getMinute(){
+        return minute;
+    }
+    public long getSecond(){
+        return second;
+    }
+    public void setTime(long elapsedTime) {
+        new Time(elapsedTime);
+    }
+
+}
