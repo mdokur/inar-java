df37e5b2ea93c542ff30ca1578c17e04ee697e8d
mdokur <m.denisokur@gmail.com>
2023-11-27 00:16:52 +0100

    correction

--- src/week_11/assigments/MyStack.java
+++ src/week_11/assigments/MyStack.java
@@ -1,2 +1,34 @@
-package week_11.assigments;public class MyStack {
+package week_11.assigments;
+
+import java.util.ArrayList;
+
+public class MyStack {
+    private ArrayList<Object> list = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public Object peek() {
+        return list.get(getSize() - 1);
+    }
+
+    public Object pop() {
+        Object o = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return o;
+    }
+
+    public void push(Object o) {
+        list.add(o);
+    }
+
+    @Override
+    public String toString() {
+        return "stack: " + list.toString();
+    }
 }
--- src/week_11/assigments/Question_11_10.java
+++ src/week_11/assigments/Question_11_10.java
@@ -1,2 +1,36 @@
-package week_11.assigments;public class Question_11_10 {
+package week_11.assigments;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Question_11_10 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = createArrayList();
+
+        System.out.println("Stack: " + list.toString());
+        System.out.println("Displaying them in reverse order...");
+        sortArrayList(list);
+        System.out.println("New Stack: " + list.toString());
+    }
+
+    public static ArrayList<Integer> createArrayList() {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println("Enter a list of integers ending with '0': ");
+        int value = -1;
+        while (value != 0) {
+            value = input.nextInt();
+            if (value != 0) {
+                list.add(value);
+            }
+        }
+        return list;
+    }
+
+    public static void sortArrayList(ArrayList<Integer> list) {
+        Collections.sort(list);
+        Collections.reverse(list);
+    }
 }
